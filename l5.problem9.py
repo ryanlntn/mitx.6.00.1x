3fbfef7434799ba02854be53f06c44e9a94c9a6c
Ryan Linton <ryanl@clearsightstudio.com>
2015-07-26 19:13:47 +0200

    Solve l5 problem 7 and add problems 8, 9 and ps3

---
+++ l5.problem9.py
@@ -0,0 +1,9 @@
+def semordnilap(str1, str2):
+    '''
+    str1: a string
+    str2: a string
+
+    returns: True if str1 and str2 are semordnilap;
+             False otherwise.
+    '''
+    # Your code here
